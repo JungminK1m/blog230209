39061797cee26b28d70bba3e1bc3d86ed5519756
unknown <deliah07@naver.com>
2023-02-09 11:30:53 +0900

    프로젝트 초기 세팅 완료

---
+++ src/test/java/shop/mtcoding/min/MinApplicationTests.java
@@ -0,0 +1,13 @@
+package shop.mtcoding.min;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
